359d8130135e1fbe225d7a230ea5f463112aef2f
Barguje Tejas Sanjay <114840943+tejasbargujepatil@users.noreply.github.com>
2025-07-20 02:52:55 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,23 @@
-// db.js
+// // db.js
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   user: 'tejas',
+//   host: 'localhost',
+//   database: 'postgres',
+//   password: 'Suhana7136',
+//   port: 5432,
+// });
+
+// module.exports = pool;
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'tejas',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'Suhana7136',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // required for Render PostgreSQL
+  },
 });
 
 module.exports = pool;
